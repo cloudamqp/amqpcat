c978a5db6369aacfa7d50aa145f3c2363b7cb6ef
Carl Hörberg <carl.hoerberg@gmail.com>
2020-09-01 23:03:42 +0200

    Improve the spec to verify that the message was routed

--- spec/amqpcat_spec.cr
+++ spec/amqpcat_spec.cr
@@ -2,6 +2,12 @@ require "./spec_helper"
 
 describe AMQPCat do
   it "works" do
+
+    client = AMQP::Client.new
+    channel = client.connect.channel
+    q = channel.queue "amqpcat-1"
+    q.purge
+
     r, w = IO.pipe
     cat = AMQPCat.new("amqp://localhost", r)
     spawn do
@@ -9,6 +15,11 @@ describe AMQPCat do
       w.puts "hello"
       w.close
     end
-    cat.produce("", "test", "direct")
+    cat.produce("", "amqpcat-1", "direct")
+
+    sleep 1
+    msg = q.get
+    msg.should_not be_nil
+    msg.not_nil!.body_io.to_s.should eq "hello"
   end
 end
